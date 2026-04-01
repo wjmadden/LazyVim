ec0b4ddd71e7f92d1dad6ec2408045e6b42b131a
Warren Madden <svurg@yahoo.com>
2026-04-01 13:54:13 -0400

    updated ledger plugin settings

--- lua/plugins/ledger.lua
+++ lua/plugins/ledger.lua
@@ -2,10 +2,15 @@ return {
   {
     "ledger/vim-ledger",
     ft = { "ledger", "journal", "hledger" },
-    config = function()
+    init = function()
       vim.g.ledger_bin = "hledger"
-      vim.g.ledger_align_at = 50
+      vim.g.ledger_is_hledger = 1
+      vim.g.ledger_align_at = 75
+      vim.g.ledger_fold_level = 1
       vim.g.ledger_default_commodity = "USD"
+      vim.g.ledger_accounts_command = "hledger accounts -f accounts.journal"
+      vim.g.ledger_fuzzy_account_completion = 1
+      vim.g.ledger_date_format = "%Y-%m-%d"
     end,
   },
 }
