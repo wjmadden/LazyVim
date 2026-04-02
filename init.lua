1c11549f3f3135fae5024a9efb676d2db8f6bd93
Warren Madden <svurg@yahoo.com>
2026-04-02 16:59:03 -0400

    change to get jsonc file type working

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Force Treesitter to use the 'json' parser for 'jsonc' files
+vim.treesitter.language.register("json", "jsonc")
