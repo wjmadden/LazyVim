4aa967e83dec8c4ac3396eba5bf60d01c007269d
Warren Madden <svurg@yahoo.com>
2026-05-25 15:19:56 -0400

    fixed account and payee completion (hopefully)

--- lua/plugins/ledger.lua
+++ lua/plugins/ledger.lua
@@ -2,13 +2,22 @@ return {
   {
     "ledger/vim-ledger",
     ft = { "ledger", "journal", "hledger" },
+    keys = {
+      {
+        "gl",
+        "<cmd>call ledger#transaction_state_toggle(line('.'), ' !*')<CR>",
+        buffer = true,
+        desc = "Find Plugin File",
+      },
+    },
     init = function()
       vim.g.ledger_bin = "hledger"
-      vim.g.ledger_is_hledger = 1
+      -- vim.g.ledger_is_hledger = true -- broken in vim-ledger
       vim.g.ledger_align_at = 75
       vim.g.ledger_fold_level = 1
       vim.g.ledger_default_commodity = "USD"
-      vim.g.ledger_accounts_command = "hledger accounts -f accounts.journal"
+      vim.g.ledger_accounts_cmd = "hledger accounts -f accounts.journal"
+      vim.g.ledger_descriptions_cmd = "hledger descriptions -f allyears.journal"
       vim.g.ledger_fuzzy_account_completion = 1
       vim.g.ledger_date_format = "%Y-%m-%d"
     end,
