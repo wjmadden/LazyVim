d8d97d81324c7ce30d876bb4b643d271a497437f
Warren Madden <svurg@yahoo.com>
2026-06-02 01:39:58 -0400

    changed blink handling of jj to do escaping better

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,18 @@
 -- return {}
 return {
   "saghen/blink.cmp",
+  keys = {
+    -- Intercepts "jj" in Insert mode, cancels blink, and leaves Insert Mode
+    {
+      "jj",
+      function()
+        require("blink.cmp").cancel()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), "n", false)
+      end,
+      mode = "i",
+      desc = "Escape and Cancel Completion",
+    },
+  },
   opts = {
     sources = {
       -- Your default active sources for ordinary files
